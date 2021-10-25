d5e9efcfcabff6a3f2e48a4f6a59146319f6742b
robinndev <robson_dev@hotmail.com>
2021-10-25 19:38:12 -0300

    adding new user view

---
+++ frontend/src/views/newUserView/NewUser.vue
@@ -0,0 +1,58 @@
+<template>
+  <body class="new-user">
+    <form class="b-form-group form-group">
+      <img
+        src="../../assets/logomarca.png"
+        class="image-logo"
+        alt="Logo da empresa"
+      />
+
+      <div role="group">
+        <b-form-input
+          class="input-name"
+          id="input-live"
+          v-model="name"
+          placeholder="Enter your name"
+          trim
+        ></b-form-input>
+
+        <b-form-select
+          class="input-select"
+          v-model="selected"
+          :options="options"
+        ></b-form-select>
+        <b-form-input
+          id="input-default"
+          placeholder="Enter your name"
+          type="password"
+        ></b-form-input>
+
+        <b-button block variant="primary" class="button-enter">Block Level Button</b-button>
+      </div>
+      
+      <div class="enter-link">
+        <h6>Enter</h6>
+        <h6>Forgot password</h6>
+      </div>
+    </form>
+  </body>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selected: null,
+      options: [
+        { value: null, text: "Please select an option" },
+        { value: "gerente", text: "Gerente" },
+        { value: "funcionario", text: "Funcionário" },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+@import "./newUser.css";
+</style>
